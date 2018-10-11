4651a5c63592bc69f0547b6e0b947435be2e2b61
Swapnil-Ingle <dr.swapnilingle@gmail.com>
2018-10-11 12:54:33 +0530

    SI: Added the datetime conditions in MS-SQL DB and retrieved it into detail object. 1. TIMEPOINT_CR_DATE 2. PROCEDURE_CR_DATE 3. TIMEPOINT_UPDATE 4. PROCEDURE_UPDATE

--- src/main/java/com/krishagni/openspecimen/msk2/importer/CarsDetail.java
+++ src/main/java/com/krishagni/openspecimen/msk2/importer/CarsDetail.java
@@ -1,5 +1,7 @@
 package com.krishagni.openspecimen.msk2.importer;
 
+import java.util.Date;
+
 public class CarsDetail {
 	
 	private String irbNumber;
@@ -17,6 +19,14 @@ public class CarsDetail {
 	private String specimenType;
 	
 	private String collectionContainer;
+	
+	private Date timepoint_Cr_Date;
+	
+	private Date timepoint_Update;
+	
+	private Date procedure_Cr_Date;
+	
+	private Date procedure_Update;
 
 	public String getIrbNumber() {
 		return irbNumber;
@@ -80,5 +90,37 @@ public class CarsDetail {
 
 	public void setCollectionContainer(String collectionContainer) {
 		this.collectionContainer = collectionContainer;
+	}
+
+	public Date getTimepoint_Cr_Date() {
+		return timepoint_Cr_Date;
+	}
+
+	public void setTimepoint_Cr_Date(Date timepoint_Cr_Date) {
+		this.timepoint_Cr_Date = timepoint_Cr_Date;
+	}
+
+	public Date getTimepoint_Update() {
+		return timepoint_Update;
+	}
+
+	public void setTimepoint_Update(Date timepoint_Update) {
+		this.timepoint_Update = timepoint_Update;
+	}
+
+	public Date getProcedure_Cr_Date() {
+		return procedure_Cr_Date;
+	}
+
+	public void setProcedure_Cr_Date(Date procedure_Cr_Date) {
+		this.procedure_Cr_Date = procedure_Cr_Date;
+	}
+
+	public Date getProcedure_Update() {
+		return procedure_Update;
+	}
+
+	public void setProcedure_Update(Date procedure_Update) {
+		this.procedure_Update = procedure_Update;
 	}
 }
